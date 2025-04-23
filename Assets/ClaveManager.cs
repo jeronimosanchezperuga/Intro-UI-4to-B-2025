4f33533e76acb4aac827dcf83c92ca9fb37e2708
Jeronimo Sanchez <jeronimo.sanchez@ort.edu.ar>
2025-04-23 15:38:57 -0300

    IngresoClave terminado

---
+++ Assets/ClaveManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ClaveManager : MonoBehaviour
+{
+    public TMP_InputField inputClave;
+    public TextMeshProUGUI txtMensaje;
+
+    string clave = "tic2025";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtMensaje.text = string.Empty;
+    }
+
+    public void ComprobarClaveIngresada()
+    {
+        string claveIngresada = inputClave.text;
+
+        if (claveIngresada != clave)
+        {
+            txtMensaje.text = "Acceso denegado";
+            inputClave.text = "";
+        }
+        else
+        {
+            txtMensaje.text = "Bienvenido";
+        }
+    }
+}
